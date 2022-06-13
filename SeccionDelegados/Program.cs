6ca9ef58f722794e803e1bc162bb26a3a6abd055
dgmrtabu <dgmr.tabu@gmail.com>
2022-06-13 15:44:36 -0400

    92 - Delegados

--- SeccionDelegados/Program.cs
+++ SeccionDelegados/Program.cs
@@ -16,10 +16,58 @@ namespace SeccionDelegados
             // DelegadosSinRetorno d1 = ImpresionTablaMultiplicar;
             //EjemploDelegado();
             //EjemploDelegadosLamda();
-            EjemploAcccion();
+            //EjemploAcccion();
+            //EjemploFunc();
+            Predicate();
             Console.Read();
         }
 
+        static void Predicate() 
+        {
+            Predicate<int> d1 = (valor) =>
+            {
+                return valor > 10;
+            };
+            Console.WriteLine($"Predicate Entero");
+            Console.WriteLine($"50:{ d1(50)}");
+            Console.WriteLine($"5: {d1(5)}");
+
+            Predicate<string> d2 = (valor) =>
+            {
+                return valor.StartsWith("A");
+            };
+            Console.WriteLine($"Predicate Entero");
+            Console.WriteLine($"ALEX:{ d2("ALEX")}");
+            Console.WriteLine($"JORGE: {d2("JORGE")}");
+            Console.WriteLine();
+
+            Func<int, bool> f1 = new Func<int, bool>(d1);
+            Console.WriteLine($"50:{ f1(50)}");
+
+            Func<string, bool> f2 = new Func<string, bool>(d2);
+            Console.WriteLine($"ALEX:{ f2("ALEX")}");
+
+            var lista = new List<int>();
+            //lista.ex
+
+            Console.WriteLine();
+        }
+        static void EjemploFunc()
+        {
+            Func<int, int> d1 = (numero) => numero * numero;
+            Console.WriteLine(d1(13));
+
+            Func<int, List<int>> d2 = (numero) => Enumerable.Range(1, 10).Select(x => x * numero).ToList();
+            var lista = d2(20);
+            Console.WriteLine(string.Join("-", lista));
+
+            lista.Select(x => x);
+            lista.Select((x,y) => x);
+            lista.Where(x => x > 0);
+            lista.Where((x, y) => x > 0);
+            lista.FirstOrDefault(x => x > 0);
+            //lista.Aggregate()
+        }
         static void EjemploAcccion()
         {
             Action<int, int> d1 = (numero, hasta) =>
@@ -49,28 +97,6 @@ namespace SeccionDelegados
             d1 = ImpresionMultiplicaicon;
             d1(15, 15);
 
-            static void EjemploDelegadosLamda()
-            {
-                DelegadosSinRetorno d1 = (numero, hasta) =>
-                {
-                    for (int i = 0; i <= hasta; i++)
-                    {
-                        Console.WriteLine($"{numero} x {i} = {numero * i}");
-                    }
-                    Console.WriteLine("-------------------------------------");
-                    Console.WriteLine($"La Tabla de Multiplicar del {numero}");
-                };
-
-                d1(3, 10);
-                d1 = (numero1, numero2) =>
-                {
-                    Console.WriteLine("Multiplicacion");
-                    Console.WriteLine($"{numero1} x {numero2} = {numero1 * numero2}");
-                    Console.WriteLine("-------------------------------------");
-                    Console.WriteLine("");
-                };
-                d1(3, 10);
-
                 DelegadosSinRetorno delegandoLamda = (numero, hasta) =>
                 {
                     Console.WriteLine($"La tabla de multiplicar {numero}");
@@ -84,10 +110,8 @@ namespace SeccionDelegados
 
                 d1 = new Action<int, int>(delegandoLamda);
                 d1(17, 5);
-            }
-
+            
         }
-
         static void EjemploDelegado() 
         {
             DelegadosSinRetorno d1 = ImpresionTablaMultiplicar;
